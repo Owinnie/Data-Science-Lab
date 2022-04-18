0f59ea926b8beffc8d24a51e7c43f07d677212ea
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-04-18 22:15:26 +0300

    Update ARmodel.py

--- 030-air-quality-in-nairobi/ARmodel.py
+++ 030-air-quality-in-nairobi/ARmodel.py
@@ -1,7 +1,8 @@
 from statsmodels.tsa.ar_model import AutoReg
 from sklearn.metrics import mean_absolute_error
 
-
+# Use AR model to predict PM2.5 readings
+# Hyperparameter --> p
 p_params = range(1, 31)
 maes = []
 for p in p_params:
@@ -17,3 +18,11 @@ for p in p_params:
     
 mae_series = pd.Series(maes, name="mae", index=p_params)
 mae_series.head()
+
+# sample output
+1    0.947888
+2    0.933894
+3    0.920850
+4    0.920153
+5    0.919519
+Name: mae, dtype: float64
